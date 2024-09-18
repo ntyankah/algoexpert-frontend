ffe1f6fc1d71e7bee461aaf6a4bfd0958da69f75
Nathaniel <yankahnathanieltopp@gmail.com>
2024-09-18 19:45:54 +0100

    add navbar

--- app/questions/page.tsx
+++ app/questions/page.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Navbar from './sections/Navbar'
+import Hero from './sections/Hero'
 
 export default function QuestionsPage() {
   return (
-    <div>
-      Questions Page initialized
+    <div >
+      <div className='min-h-[540px] bg-gradient-to-r from-[#02203c] to-[#001528] text-white'>
+        <div className='container mx-auto h-full flex flex-col'>
+          <Navbar />
+          <Hero />
+        </div>
+      </div>
     </div>
   )
 }
